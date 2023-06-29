2d824338a2b113d35ed1c2f1f63a9b6e6cfd0328
Mohamad <alkhatib.m804@gmail.com>
2023-06-29 23:51:22 +0200

    update routes

--- src/App.vue
+++ src/App.vue
@@ -2,48 +2,63 @@
 	<div id="app">
 		<header>
 			<router-link to="/">
-				<img src="../public/MaxAirain2.png"
+				<img
+					src="../public/MaxAirain2.png"
 					alt="Logo"
-					class="logo" />
+					class="logo"
+				/>
 			</router-link>
 			<div class="title-container">
 				<h1 class="title">Max Airain</h1>
 			</div>
 			<div class="header-right">
-
 				<nav v-if="!authToken">
-					<router-link to="/inscription"
+					<router-link
+						to="/inscription"
 						class="nav-button"
-						:class="{ 'nav-button-active': $route.path === '/inscription' }">
+						:class="{ 'nav-button-active': $route.path === '/inscription' }"
+					>
 						S'inscrire
 					</router-link>
-					<router-link to="/connexion"
+					<router-link
+						to="/connexion"
 						class="nav-button"
-						:class="{ 'nav-button-active': $route.path === '/connexion' }">
-						<svg xmlns="http://www.w3.org/2000/svg"
+						:class="{ 'nav-button-active': $route.path === '/connexion' }"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
 							width="16"
 							height="16"
 							fill="currentColor"
 							class="bi bi-person-fill"
-							viewBox="0 0 16 16">
+							viewBox="0 0 16 16"
+						>
 							<path
-								d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+								d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+							/>
 						</svg>
 						Se connecter
 					</router-link>
 				</nav>
-				<div class="welcome"
-					v-else>
-					<button class="nav-button"
-						@click="logout">
-						<svg xmlns="http://www.w3.org/2000/svg"
+				<div
+					class="welcome"
+					v-else
+				>
+					<button
+						class="nav-button"
+						@click="logout"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
 							width="16"
 							height="16"
 							fill="currentColor"
 							class="bi bi-person-fill"
-							viewBox="0 0 16 16">
+							viewBox="0 0 16 16"
+						>
 							<path
-								d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+								d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+							/>
 						</svg>
 						Déconnexion
 					</button>
@@ -55,101 +70,94 @@
 </template>
 
 <script>
-	import {
-		useRouter
-	} from 'vue-router';
-	import {
-		defineComponent,
-		computed,
-	} from 'vue';
-	import {
-		useStore
-	} from 'vuex';
-	export default defineComponent({
-		name: 'App',
-		setup() {
-			const router = useRouter();
-			const store = useStore();
-			const authToken = computed(() => store.state.authToken);
-			const logout = () => {
-				store.commit('SET_AUTH_TOKEN', null);
-				localStorage.removeItem('authToken');
-				router.push('/');
-			};
-			return {
-				authToken,
-				logout
-			};
-		}
-	});
+import { useRouter } from 'vue-router';
+import { defineComponent, computed } from 'vue';
+import { useStore } from 'vuex';
+export default defineComponent({
+	name: 'App',
+	setup() {
+		const router = useRouter();
+		const store = useStore();
+		const authToken = computed(() => store.state.authToken);
+		const logout = () => {
+			store.commit('SET_AUTH_TOKEN', null);
+			localStorage.removeItem('authToken');
+			router.push('/');
+		};
+		return {
+			authToken,
+			logout,
+		};
+	},
+});
 </script>
 
 <style>
-	#app {
-		text-align: center;
-		font-family: 'Roboto', sans-serif;
-	}
+#app {
+	text-align: center;
+	font-family: 'Roboto', sans-serif;
+}
 
-	header {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		background-color: #f5f5f5;
-		padding: 20px;
-		position: relative;
-	}
+header {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	background-color: #f5f5f5;
+	padding: 20px;
+	position: relative;
+}
 
-	.logo {
-		max-width: 200px;
-	}
+.logo {
+	max-width: 200px;
+}
 
-	.header-right {
-		display: flex;
-		align-items: center;
-	}
+.header-right {
+	display: flex;
+	align-items: center;
+}
 
-	.title-container {
-		flex: 1;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
+.title-container {
+	flex: 1;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
 
-	.title {
-		margin: 0 auto;
-		color: #379EC1;
-	}
+.title {
+	margin: 0 auto;
+	color: #379ec1;
+}
 
-	nav {
-		margin-left: 20px;
-	}
+nav {
+	margin-left: 20px;
+}
 
-	.nav-button {
-		text-decoration: none;
-		color: black;
-		border: 1px solid black;
-		padding: 5px;
-		transition: background-color 0.3s ease;
-		border-radius: 8px;
-		margin-left: 10px;
-	}
+.nav-button {
+	text-decoration: none;
+	color: black;
+	border: 1px solid black;
+	padding: 5px;
+	transition: background-color 0.3s ease;
+	border-radius: 8px;
+	margin-left: 10px;
+}
 
-	.nav-button:hover {
-		background-color: #57bbdc;
-		color: white;
-	}
+.nav-button:hover {
+	background-color: #57bbdc;
+	color: white;
+}
 
-	.nav-button-active {
-		background-color: #379EC1;
-		color: white;
-	}
+.nav-button-active {
+	background-color: #379ec1;
+	color: white;
+}
 
-	.welcome .nav-button:hover {
-		background-color: red;
-		color: white;
-	}
+.welcome .nav-button:hover {
+	background-color: red;
+	color: white;
+}
 
-	.welcome {
-		margin-left: auto;
-	}
+.welcome {
+	margin-left: auto;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -9,44 +9,45 @@ import FaceComparaison from './components/FaceComparaison.vue';
 import Materials from './components/MaterialList.vue';
 
 const routes = [
-  {
-    path: '/',
-    redirect: () => {
-      // Si l'utilisateur est déconnecté, redirige vers /materials
-      return store.state.authToken ? '/' : '/materials';
-    },
-  },
-  {
-    path: '/inscription',
-    component: Inscription
-  },
-  {
-    path: '/connexion',
-    component: Connexion
-  },
-  {
-    path: '/material',
-    component: MaterialInput
-  },
-  {
-    path: '/facecompare',
-    component: FaceComparaison
-  },
-  {
-    path: '/materials',
-    component: Materials
-  },
+	{
+		path: '/',
+		redirect: () => {
+			// Si l'utilisateur est déconnecté, redirige vers /materials
+			return store.state.authToken ? '/' : '/materials';
+		},
+	},
+	{
+		path: '/inscription',
+		component: Inscription,
+	},
+	{
+		path: '/connexion',
+		component: Connexion,
+	},
+	{
+		path: '/material',
+		component: MaterialInput,
+	},
+	{
+		path: '/facecompare',
+		component: FaceComparaison,
+	},
+	{
+		path: '/materials',
+		component: Materials,
+	},
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+	history: createWebHistory(),
+	base: process.env.BASE_URL,
+	routes,
 });
 
 const authToken = localStorage.getItem('authToken');
 if (authToken) {
-  // Si un token est trouvé, le mettre dans le store Vuex
-  store.commit('SET_AUTH_TOKEN', authToken);
+	// Si un token est trouvé, le mettre dans le store Vuex
+	store.commit('SET_AUTH_TOKEN', authToken);
 }
 
 createApp(App).use(router).use(store).mount('#app');
