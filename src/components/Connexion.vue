322de6208f2a549d21a7ad2292e5b284869218d2
Mohamad <alkhatib.m804@gmail.com>
2023-06-29 20:37:02 +0200

    Upload image

--- src/components/Connexion.vue
+++ src/components/Connexion.vue
@@ -1,108 +1,104 @@
 <script setup>
-	import {
-		ref,
-		onMounted
-	} from 'vue';
-	import {
-		useRouter
-	} from 'vue-router';
-	import api from '../config/api';
-	import * as faceapi from 'face-api.js';
-	import {
-		useStore
-	} from 'vuex';
-	const router = useRouter();
-	const store = useStore();
-	const email = ref('');
-	const photoTaken = ref(false);
-	console.log(process.env.VUE_APP_API_URL);
-	const submitForm = async () => {
-		const {
-			descriptor
-		} = await getFacialInformation();
+import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import api from '../config/api';
+import * as faceapi from 'face-api.js';
+import { useStore } from 'vuex';
+const router = useRouter();
+const store = useStore();
+const email = ref('');
+const photoTaken = ref(false);
+const videoRef = ref(null);
+const canvasRef = ref(null);
+const image = ref(null);
+
+const submitForm = async () => {
+	try {
+		console.log(canvasRef.value.toDataURL('image/jpeg'));
+		const { descriptor } = await getFacialInformation();
 		const descriptorToString = JSON.stringify(Array.from(descriptor));
-		try {
-			const response = await api.post('v1/auth/login', {
-				email: email.value,
-				descriptor: descriptorToString,
-			});
-			console.log(response.data);
-			store.commit('SET_AUTH_TOKEN', response.data.token);
-			// Stocker le token dans le localStorage
-			localStorage.setItem('authToken', response.data.token);
-			router.push('/material');
-			photoTaken.value = false;
-		} catch (error) {
-			console.error(error);
-			alert("Visage non reconnu")
-		}
-	};
-	const videoRef = ref(null);
-	const canvasRef = ref(null);
-	const image = ref(null);
-	onMounted(async () => {
-		console.log('loading model');
-		try {
-			await faceapi.nets.ssdMobilenetv1.loadFromUri('/models');
-			await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
-			await faceapi.nets.faceRecognitionNet.loadFromUri('/models');
-		} catch (error) {
-			console.error('Failed to load model:', error);
-		}
-		console.log('model loaded');
-		navigator.mediaDevices
-			.getUserMedia({
-				video: true,
-			})
-			.then((stream) => {
-				// console.log('got stream', stream);
-				videoRef.value.srcObject = stream;
-			})
-			.catch((err) => {
-				console.error('error starting video stream', err);
-			});
-	});
-	const updateCanvasDimensions = () => {
-		canvasRef.value.width = videoRef.value.videoWidth;
-		canvasRef.value.height = videoRef.value.videoHeight;
-	};
-	const capture = () => {
-		const context = canvasRef.value.getContext('2d');
-		context.drawImage(
-			videoRef.value,
-			0,
-			0,
-			videoRef.value.videoWidth,
-			videoRef.value.videoHeight
-		);
-		const imageData = context.getImageData(
-			0,
-			0,
-			canvasRef.value.width,
-			canvasRef.value.height
-		);
-		image.value = imageData;
-		photoTaken.value = true;
-	};
-	const getFacialInformation = async () => {
-		if (!image.value) {
-			console.error('need one image');
-			return;
-		}
-		if (!faceapi.nets.ssdMobilenetv1.params) {
-			throw new Error('ssdMobilenetv1 model not loaded');
-		}
-		const canvas = document.createElement('canvas');
-		const context = canvas.getContext('2d');
-		canvas.width = image.value.width;
-		canvas.height = image.value.height;
-		context.putImageData(image.value, 0, 0);
-		const detection = await faceapi
-			.detectSingleFace(canvas)
-			.withFaceLandmarks()
-			.withFaceDescriptor();
-		return detection;
-	};
+
+		const response = await api.post('v1/auth/login', {
+			email: email.value,
+			descriptor: descriptorToString,
+		});
+
+		store.commit('SET_AUTH_TOKEN', response.data.token);
+		// Stocker le token dans le localStorage
+		localStorage.setItem('authToken', response.data.token);
+		router.push('/material');
+		photoTaken.value = false;
+	} catch (error) {
+		console.error(error);
+		alert('Visage non reconnu');
+	}
+};
+
+onMounted(async () => {
+	console.log('loading model');
+	try {
+		await faceapi.nets.ssdMobilenetv1.loadFromUri('/models');
+		await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
+		await faceapi.nets.faceRecognitionNet.loadFromUri('/models');
+	} catch (error) {
+		console.error('Failed to load model:', error);
+	}
+	console.log('model loaded');
+	navigator.mediaDevices
+		.getUserMedia({
+			video: true,
+		})
+		.then((stream) => {
+			// console.log('got stream', stream);
+			videoRef.value.srcObject = stream;
+		})
+		.catch((err) => {
+			console.error('error starting video stream', err);
+		});
+});
+
+const updateCanvasDimensions = () => {
+	canvasRef.value.width = videoRef.value.videoWidth;
+	canvasRef.value.height = videoRef.value.videoHeight;
+};
+
+const capture = () => {
+	const context = canvasRef.value.getContext('2d');
+	context.drawImage(
+		videoRef.value,
+		0,
+		0,
+		videoRef.value.videoWidth,
+		videoRef.value.videoHeight
+	);
+	const imageData = context.getImageData(
+		0,
+		0,
+		canvasRef.value.width,
+		canvasRef.value.height
+	);
+	image.value = imageData;
+	photoTaken.value = true;
+};
+const getFacialInformation = async () => {
+	if (!image.value) {
+		console.error('need one image');
+		return;
+	}
+	if (!faceapi.nets.ssdMobilenetv1.params) {
+		throw new Error('ssdMobilenetv1 model not loaded');
+	}
+	const canvas = document.createElement('canvas');
+	const context = canvas.getContext('2d');
+	canvas.width = image.value.width;
+	canvas.height = image.value.height;
+	context.putImageData(image.value, 0, 0);
+	const detection = await faceapi
+		.detectSingleFace(canvas)
+		.withFaceLandmarks()
+		.withFaceDescriptor();
+	return detection;
+};
 </script>
 
 <template>
@@ -111,46 +107,62 @@
 		<form @submit.prevent="submitForm">
 			<div class="form-group">
 				<label for="email">Email:</label>
-				<input type="email"
+				<input
+					type="email"
 					id="email"
-					v-model="email" />
+					v-model="email"
+				/>
 			</div>
 
 			<div class="container">
 				<div class="videos">
-					<video ref="videoRef"
+					<video
+						ref="videoRef"
 						@playing="updateCanvasDimensions"
-						autoplay></video>
+						autoplay
+					></video>
 					<div class="centered-text">
 						<h1>Capturez votre photo</h1>
 					</div>
 					<canvas ref="canvasRef"></canvas>
 				</div>
 				<div class="buttons">
-					<button type="button"
-						@click="capture">
-						<svg xmlns="http://www.w3.org/2000/svg"
+					<button
+						type="button"
+						@click="capture"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
 							width="16"
 							height="16"
 							fill="currentColor"
 							class="bi bi-camera-fill"
-							viewBox="0 0 16 16">
+							viewBox="0 0 16 16"
+						>
 							<path d="M10.5 8.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
 							<path
-								d="M2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2zm.5 2a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm9 2.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z" />
-						</svg> Capture
+								d="M2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2zm.5 2a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm9 2.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z"
+							/>
+						</svg>
+						Capture
 					</button>
-					<button type="submit"
-						:disabled="!photoTaken">
-						<svg xmlns="http://www.w3.org/2000/svg"
+					<button
+						type="submit"
+						:disabled="!photoTaken"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
 							width="16"
 							height="16"
 							fill="currentColor"
 							class="bi bi-person-fill"
-							viewBox="0 0 16 16">
+							viewBox="0 0 16 16"
+						>
 							<path
-								d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
-						</svg> Se connecter
+								d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+							/>
+						</svg>
+						Se connecter
 					</button>
 				</div>
 			</div>
@@ -159,80 +171,80 @@
 </template>
 
 <style scoped>
-	.container {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-	}
+.container {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
 
-	.videos {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		width: 100%;
-		margin-top: 20px;
-		background: linear-gradient(135deg, #379EC1, #769F2B);
-		color: whitesmoke;
-	}
+.videos {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 100%;
+	margin-top: 20px;
+	background: linear-gradient(135deg, #379ec1, #769f2b);
+	color: whitesmoke;
+}
 
-	.buttons {
-		display: flex;
-		justify-content: center;
-		margin-top: 10px;
-	}
+.buttons {
+	display: flex;
+	justify-content: center;
+	margin-top: 10px;
+}
 
-	button {
-		margin: 0 10px;
-		padding: 10px 20px;
-		font-size: 16px;
-		color: #fff;
-		border: none;
-		cursor: pointer;
-		transition: background-color 0.3s ease;
-		background-color: #379EC1;
-		border-radius: 5px;
-	}
+button {
+	margin: 0 10px;
+	padding: 10px 20px;
+	font-size: 16px;
+	color: #fff;
+	border: none;
+	cursor: pointer;
+	transition: background-color 0.3s ease;
+	background-color: #379ec1;
+	border-radius: 5px;
+}
 
-	button:disabled {
-		background-color: #ccc;
-		cursor: not-allowed;
-	}
+button:disabled {
+	background-color: #ccc;
+	cursor: not-allowed;
+}
 
-	.centered-text {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		height: 100%;
-		text-align: center;
-	}
+.centered-text {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 100%;
+	text-align: center;
+}
 
-	button:hover {
-		background-color: #50b6d8;
-	}
+button:hover {
+	background-color: #50b6d8;
+}
 
-	.form-group {
-		margin-bottom: 20px;
-		width: 50%;
-		margin: 0 auto;
-	}
+.form-group {
+	margin-bottom: 20px;
+	width: 50%;
+	margin: 0 auto;
+}
 
-	label {
-		display: block;
-		margin-bottom: 5px;
-		font-weight: bold;
-	}
+label {
+	display: block;
+	margin-bottom: 5px;
+	font-weight: bold;
+}
 
-	input[type="email"] {
-		width: 100%;
-		padding: 10px;
-		font-size: 16px;
-		border: 1px solid #ccc;
-		border-radius: 5px;
-		box-sizing: border-box;
-	}
+input[type='email'] {
+	width: 100%;
+	padding: 10px;
+	font-size: 16px;
+	border: 1px solid #ccc;
+	border-radius: 5px;
+	box-sizing: border-box;
+}
 
-	h2 {
-		text-align: center;
-		margin-bottom: 20px;
-	}
+h2 {
+	text-align: center;
+	margin-bottom: 20px;
+}
 </style>
